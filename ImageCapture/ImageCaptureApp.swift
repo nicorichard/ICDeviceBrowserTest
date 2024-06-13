53604f1648b461c0a64a18608c202519b60fdfc9
Nico Richard <nicolas.richard@justeattakeaway.com>
2024-06-13 16:21:44 -0500

    Initial Commit

---
+++ ImageCapture/ImageCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCaptureApp.swift
+//  ImageCapture
+//
+//  Created by Nicolas Richard on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct ImageCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
